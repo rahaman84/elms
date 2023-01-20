d1493bfeed5379d11813df0f5d4f3463945f1571
Rahaman84 <rahamanarifur_84@hotmail.com>
2023-01-20 13:51:25 +0600

    Make Topbanner

---
+++ src/components/TopBanner/TopBanner.jsx
@@ -0,0 +1,31 @@
+import React, { Component, Fragment } from 'react'
+import { Button, Col, Container, Row } from 'react-bootstrap'
+import '../../asset/css/custom.css';
+import '../../asset/css/bootstrap.min.css';
+
+ class TopBanner extends Component {
+  render() {
+    return (
+      
+     <Fragment>
+        <Container fluid={true} className="topFixedBanner p-0">
+            <div className='topFixedBannerOverlay'>
+
+              <Container className='topContent'>
+                <Row>
+                  <Col className='text-center'>
+                  <h1 className='topTitle'>E-Learning Platform</h1>
+                  <h4 className='topSubTitle'>Learn Professionally </h4>               
+                   <Button variant="danger">Learn more.</Button>{' '}
+                  </Col>
+                </Row>
+              </Container>
+
+            </div>
+        </Container>
+     </Fragment>
+    )
+  }
+}
+
+export default TopBanner
